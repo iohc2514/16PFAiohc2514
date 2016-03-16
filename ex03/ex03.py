1161d497683414bc9b42d622db17fcf71ec8340e
CAD Client <CAD Client>
2016-03-16 19:48:46 +0900

    ex03  Study Drills

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -2,13 +2,13 @@ print "I will now count my chickens:"
 
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
-
+# 75 % 4 = 3
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+print 3 + 2 + 1 - 5 + 4 % 2 - 0.25 + 6
+# Study Drills: 1 / 4 = 0.25
 print "Is it true that 3 + 2 < 5 - 7?"
-
+# 5 < -2 Is false
 print 3 + 2 < 5 - 7
 
 print "What is 3 + 2?", 3 + 2
