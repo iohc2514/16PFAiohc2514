ad77936c42797bbde7815a997dfba9224c12413f
CAD Client <CAD Client>
2016-03-23 19:10:36 +0900

    cm에서 incs로 단위 변화

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -2,6 +2,8 @@
 my_name = 'Seokhyeon Choi'
 my_age = 23 # not a lie
 my_height = 164.5 # cm
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height *cm_to_inch
 my_weight = 63 # kg
 my_eyes = 'brown'
 my_teeth = 'White'
@@ -9,6 +11,7 @@ my_hair = 'black'
 # %g=소수점 까지%d=정수
 print "Let's talk about %s." % my_name
 print "He's %g cm tall." % my_height
+print "He's %g inches tall." %my_height_inch
 print "He's %d kg heavy." % my_weight
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
