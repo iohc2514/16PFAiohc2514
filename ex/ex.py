5ff67081836c66b1cd6d3af20259dbc923924287
CAD Client <CAD Client>
2016-05-25 20:54:31 +0900

    마지막

--- ex/ex.py
+++ ex/ex.py
@@ -1,5 +1,20 @@
-n=0
-for i in range(1,1000) :
-    n=n+1
-    if 3*n:
-        print n
+#_*_coding:cp949
+sum_square = 0
+square_sum = 0
+for a in range(1,101) :
+# for문 1부터  100까지
+    sum_square = sum_square + a
+    square_sum = square_sum + a * a
+sum_square = sum_square * sum_square
+print "sum_square = ", sum_square
+print "square_sum = ", square_sum
+difference = sum_square - square_sum
+print "sum_square - square_sum = " ,difference
+
+ 
+
+#합들의 제곱은 25502500
+
+#제곱들의 합은 338350
+
+#따라서 두 값의 차는 25164150입니다.
